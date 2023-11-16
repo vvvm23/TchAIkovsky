bcbaec04be2c4da920afae174a39ea45ce534869
Alex McKinney <alex.f.mckinney@gmail.com>
2023-11-16 10:24:40 +0000

    adds saving of midi and prompt loading

--- generate.py
+++ generate.py
@@ -1,14 +1,17 @@
 import json
 from argparse import ArgumentParser
+from datetime import datetime
 from pathlib import Path
 from types import SimpleNamespace
 
 import equinox as eqx
 import jax
 import jax.numpy as jnp
+import numpy as np
 import orbax.checkpoint as ocp
 import tqdm
 from loguru import logger
+from miditoolkit import MidiFile
 
 from data.tokenizer import get_pretrained_tokenizer
 from model import TchAIkovskyModel
@@ -27,28 +30,29 @@ def generate_step(model, inputs, key, temperature):
     pass
 
 
-def generate_loop(model, initial_input, temperature, max_to_generate):
-    pass
+def generate_loop(model, initial_input, temperature, max_to_generate, key):
+    return initial_input
 
 
 # tokenizes initial prompt
-def tokenize_prompt(midi_path):
-    pass
+def tokenize_prompt(midi, tokenizer):
+    return tokenizer(midi)
 
 
-# returns string version of uncondtional prompt
+# returns midi version of uncondtional prompt
 def unconditional_prompt():
     pass
 
 
-# return string version of chord prompt
+# return midi version of chord prompt
 def chord_prompt(chord):
     pass
 
 
 # loads prompt MIDI file
 def file_prompt(path):
-    pass
+    midi = MidiFile(path)
+    return midi
 
 
 def main(args):
@@ -92,6 +96,34 @@ def main(args):
         )
         logger.info(f"Model has {num_parameters:,} parameters.")
 
+    if args.prompt_mode == "unconditional":
+        raise NotImplementedError("Unconditional generation not implemented yet.")
+    elif args.prompt_mode == "chord":
+        raise NotImplementedError("Chord-conditioned generation not implemented yet.")
+    elif args.prompt_mode == "file":
+        logger.info(f"Loading prompt file '{args.prompt_midi}'")
+        midi = file_prompt(args.prompt_midi)
+        logger.info(midi)
+
+    logger.info("Tokenising prompt.")
+    start_tokens = np.array(tokenize_prompt(midi, tokenizer))
+    logger.info(f"Tokenised prompt is of length {start_tokens.shape[1]}")
+
+    if start_tokens.shape[1] >= config.max_sequence_length:
+        logger.warning("Tokenised prompt provided is longer than maximum length supported by model.")
+        logger.warning("Terminating")
+        return
+
+    generated_tokens = generate_loop(model, start_tokens, args.temperature, args.max_to_generate, key)
+    generated_midi = tokenizer(generated_tokens)
+
+    if args.output_file is None:
+        output_dir = Path("generated_midis")
+        output_dir.mkdir(exist_ok=True)
+        args.output_file = output_dir / (datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mid")
+
+    generated_midi.dump(args.output_file)
+
 
 def validate_args(args):
     if args.config is None:
@@ -109,10 +141,13 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=0xFF)
     parser.add_argument("--config", "-c", type=str, default=None)
     parser.add_argument("--checkpoint", type=str, default=None)
-    parser.add_argument("--prompt_mode", type=str, default="unconditional")
+    parser.add_argument("--prompt_mode", type=str, default="file")
     parser.add_argument("--prompt_midi", type=str, default=None)
     parser.add_argument("--prompt_chord", type=str, default=None)
     parser.add_argument("--tokenizer", type=str, default="tokenizer.json")
+    parser.add_argument("--temperature", type=float, default=1.0)
+    parser.add_argument("--max_to_generate", type=int, default=None)
+    parser.add_argument("--output_file", type=str, default=None)
     args = parser.parse_args()
     validate_args(args)
     main(args)
